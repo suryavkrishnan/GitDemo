2b46955fd36f3c5877f1834b5dd72965517bb930
surya <suryagayathri1981@gmail.com>
2023-07-16 00:24:29 -0400

    gitstuff commit in master branch for conflict

---
+++ src/test/java/StepDefenitions/demo.java
@@ -0,0 +1,11 @@
+package StepDefenitions;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is in gitstuff for merge colflict in master --------1-------");
+
+	}
+
+}
